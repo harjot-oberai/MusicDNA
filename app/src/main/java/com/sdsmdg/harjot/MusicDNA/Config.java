4b8742c701a48949a01e31ae56b5919c55ef2ca5
harjot-oberai <harjot.oberai@gmail.com>
2016-11-30 01:11:03 +0530

    untracked file Config.java

--- app/src/main/java/com/sdsmdg/harjot/MusicDNA/Config.java
+++
@@ -1,9 +0,0 @@
-package com.sdsmdg.harjot.MusicDNA;
-
-/**
- * Created by Harjot on 30-Apr-16.
- */
-public class Config {
-    public static final String CLIENT_ID = "YOUR_SOUNDCLOUD_CLIENT_ID";
-    public static final String API_URL = "https://api.soundcloud.com";
-}
